c4bc913cac070658cf583c696de1a5744eef0b88
Wellington Ideao <wellington.ideao@gmail.com>
2021-06-25 11:56:49 -0300

    Refactored post_list view

--- blog/views.py
+++ blog/views.py
@@ -1,22 +1,14 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.core.paginator import Paginator
 
 
 def post_list(request):
-    objects_list = Post.published.all()
-    paginator = Paginator(objects_list, 3)  # 3 posts in each page
-    page = request.GET.get('page')
-    try:
-        posts = paginator.page(page)
-    except PageNotAnInteger:
-        # If page is not an integer deliver the first page
-        posts = paginator.page(1)
-    except EmptyPage:
-        # If page is out of range deliver last page of results
-        posts = paginator.page(paginator.num_pages)
-
-    return render(request, 'blog/post/list.html', {'page': page, 'posts': posts})
+    post_pub_list = Post.published.all()
+    paginator = Paginator(post_pub_list, 3)  # 3 posts in each page
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    return render(request, 'blog/post/list.html', {'posts': page_obj})
 
 
 def post_detail(request, year, month, day, post):
