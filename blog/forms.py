069a86e836a1d188b060e5a8f97dd00eb6d5afed
Wellington Ideao <wellington.ideao@gmail.com>
2021-06-25 15:23:30 -0300

    Add email post form

---
+++ blog/forms.py
@@ -0,0 +1,8 @@
+from django.forms import forms
+
+
+class EmailPostForm(forms.Form):
+    name = forms.CharField(max_length=25)
+    email = forms.EmailField()
+    to = forms.EmailField()
+    comments = forms.CharField(require=False, widget=forms.Textarea)
