2565fb388accc0a536bf7ed4061f5aef67a007c4
Wellington Ideao <wellington.ideao@gmail.com>
2021-06-26 12:49:39 -0300

    Add CommentModelAdmin

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
 
 
 @admin.register(Post)
@@ -12,3 +12,9 @@ class PostModelAdmin(admin.ModelAdmin):
     date_hierarchy = 'publish'
     ordering = ('status', 'publish')
 
+
+@admin.register(Comment)
+class CommentModelAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'post', 'created', 'active')
+    list_filter = ('active', 'created', 'updated')
+    search_fields = ('name', 'email', 'body')
