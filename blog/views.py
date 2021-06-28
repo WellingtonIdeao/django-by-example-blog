aa8cff74b8c0dc45900b2415129bcf7bf9ef4db2
Wellington Ideao <wellington.ideao@gmail.com>
2021-06-28 17:39:54 -0300

    Add searching against multiple fields

--- blog/views.py
+++ blog/views.py
@@ -7,6 +7,7 @@ from django.core.mail import send_mail
 from taggit.models import Tag
 from django.core.paginator import Paginator, EmptyPage
 from django.db.models import Count
+from django.contrib.postgres.search import SearchVector
 
 
 class PostListView(ListView):
@@ -116,6 +117,6 @@ def post_search(request):
         form = SearchForm(request.GET)
         if form.is_valid():
             query = form.cleaned_data['query']
-            results = Post.published.filter(body__search=query)
+            results = Post.published.annotate(search=SearchVector('title', 'body'), ).filter(search=query)
 
     return render(request, 'blog/post/search.html', {'form': form, 'results': results, 'query': query})
