e8985ccedff1d90184e4229eba4f21c37d86dbcc
Wellington Ideao <wellington.ideao@gmail.com>
2021-06-28 17:47:24 -0300

    Add search with stemming and ranking results

--- blog/views.py
+++ blog/views.py
@@ -7,7 +7,7 @@ from django.core.mail import send_mail
 from taggit.models import Tag
 from django.core.paginator import Paginator, EmptyPage
 from django.db.models import Count
-from django.contrib.postgres.search import SearchVector
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
 
 
 class PostListView(ListView):
@@ -117,6 +117,9 @@ def post_search(request):
         form = SearchForm(request.GET)
         if form.is_valid():
             query = form.cleaned_data['query']
-            results = Post.published.annotate(search=SearchVector('title', 'body'), ).filter(search=query)
+            search_vector = SearchVector('title', 'body')
+            search_query = SearchQuery(query)
+            results = Post.published.annotate(search=search_vector, rank=SearchRank(search_vector, search_query)
+                                              ).filter(search=search_query).order_by('-rank')
 
     return render(request, 'blog/post/search.html', {'form': form, 'results': results, 'query': query})
