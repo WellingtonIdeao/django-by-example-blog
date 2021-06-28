4d4099d0361869114d11f70f249e59be7ba700f3
Wellington Ideao <wellington.ideao@gmail.com>
2021-06-28 15:02:50 -0300

    Add lasted posts feed

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from .views import PostListView, PostDetailView, post_share, post_detail, post_list
+from .feeds import LatestPostsFeed
+
 app_name = 'blog'
 urlpatterns = [
     # path('', PostListView.as_view(), name='post_list'),
@@ -7,4 +9,5 @@ urlpatterns = [
     path('<int:year>/<int:month>/<int:day>/<slug:slug>/', post_detail, name='post_detail'),
     path('<int:post_id>/share/', post_share, name='post_share'),
     path('tag/<slug:tag_slug>/', post_list, name='post_list_by_tag'),
+    path('feed/', LatestPostsFeed(), name='post_feed'),
 ]
